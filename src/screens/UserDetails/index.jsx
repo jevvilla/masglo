552dbddcf77d982ce984d591ac8fddd3f7085dd7
jevvilla <juan.es-19@hotmail.com>
2019-12-01 07:41:41 -0500

    navigationOptions applied to UserDetails screen

--- src/screens/UserDetails/index.jsx
+++ src/screens/UserDetails/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import * as strings from '../../common/strings';
+
+import styles from './styles';
+
 const UserDetails = () => {
   return (
     <View>
       <Text>UserDetails</Text>
     </View>
   );
+};
+
+UserDetails.navigationOptions = {
+  headerTitle: <Text style={styles.headerTitle}>{strings.USERDETAILS_TITLE_TEXT}</Text>,
 };
 
 export default UserDetails;
---
+++ src/screens/UserDetails/styles.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+
+import { hearderTitle } from '../../common/styles';
+
+export default StyleSheet.create({
+  headerTitle: {
+    ...hearderTitle,
+  },
+});
