d70995a2765767fad6a7385a5d5767ef29e69c61
jevvilla <juan.es-19@hotmail.com>
2019-12-01 07:42:41 -0500

    CardItem updated, touch option added to navigate to Details screen

--- src/screens/UserOverview/component/CardItem/index.jsx
+++ src/screens/UserOverview/component/CardItem/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Text, View, Image, ViewPropTypes } from 'react-native';
+import { Text, View, Image, ViewPropTypes, TouchableOpacity } from 'react-native';
 
 import styles from './styles';
 
@@ -10,16 +10,17 @@ const CardItem = props => {
     picture,
     style: cstyles = {},
     location: { state, country },
+    onCardPress,
   } = props;
 
   return (
-    <View style={[styles.container, cstyles]}>
+    <TouchableOpacity style={[styles.container, cstyles]} activeOpacity={0.7} onPress={onCardPress}>
       <Image source={{ uri: picture.large }} style={styles.thumbnail} />
       <View style={styles.infoContainer}>
         <Text style={styles.name}>{`${first} ${last}`}</Text>
         <Text style={styles.email}>{`${state}, ${country}`}</Text>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
 
@@ -28,15 +29,20 @@ CardItem.propTypes = {
     first: PropTypes.string,
     last: PropTypes.string,
   }).isRequired,
+
   picture: PropTypes.shape({
     large: PropTypes.string,
   }).isRequired,
+
   location: PropTypes.shape({
     state: PropTypes.string,
     country: PropTypes.string,
   }).isRequired,
+
   style: PropTypes.oneOfType([PropTypes.arrayOf(ViewPropTypes.style), ViewPropTypes.style])
     .isRequired,
+
+  onCardPress: PropTypes.func.isRequired,
 };
 
 export default CardItem;
