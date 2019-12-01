77a8a0397239d01963027116b8d20a270d2ece18
jevvilla <juan.es-19@hotmail.com>
2019-12-01 09:34:00 -0500

    IconButton component created to display touchable icons

---
+++ src/common/components/IconButton/styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+import { sizes } from '../../styles';
+
+export default StyleSheet.create({
+  container: {
+    padding: sizes.base / 2,
+    borderRadius: 50,
+    margin: sizes.base / 2,
+  },
+});
