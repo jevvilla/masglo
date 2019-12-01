5a8058b79b293f0c5504c9ddae1452ddc6726250
jevvilla <juan.es-19@hotmail.com>
2019-12-01 09:36:34 -0500

    headerLeft touchable icon to goback

--- src/screens/UserDetails/index.jsx
+++ src/screens/UserDetails/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { View, Text } from 'react-native';
 
+import { IconButton } from '../../common/components';
 import * as strings from '../../common/strings';
 
 import styles from './styles';
@@ -13,8 +14,9 @@ const UserDetails = () => {
   );
 };
 
-UserDetails.navigationOptions = {
+UserDetails.navigationOptions = ({ navigation }) => ({
   headerTitle: <Text style={styles.headerTitle}>{strings.USERDETAILS_TITLE_TEXT}</Text>,
-};
+  headerLeft: <IconButton name="arrow-left" onPress={() => navigation.goBack()} />,
+});
 
 export default UserDetails;
