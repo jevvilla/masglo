b403c80d60374a28c98d0af24c75f6eedabef637
jevvilla <juan.es-19@hotmail.com>
2019-12-01 09:32:16 -0500

    status bar styled properly

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { StatusBar } from 'react-native';
 import { Provider } from 'react-redux';
 
 import Router from './common/navigation';
 import store from './redux/store';
+import { colors } from './common/styles';
 
 const App = () => (
   <Provider store={store}>
+    <StatusBar backgroundColor={colors.background} barStyle="dark-content" />
     <Router />
   </Provider>
 );
